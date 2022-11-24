6203e13fd87bf12ab67aa309ceba1440d8e083e5
andrespaladines <andrespaladines@hotmail.com>
2022-11-24 08:46:28 -0500

    add: First and last commit (maybe)

---
+++ UIFontName/UIFontName.swift
@@ -0,0 +1,21 @@
+//
+//  UIFontName.swift
+//  UIFontName
+//
+//  Created by andres paladines on 11/24/22.
+//
+
+import Foundation
+
+class UIFontName {
+    static let shared = UIFontName()
+
+    var openSansSemiBold: String {
+        return "OpenSans-SemiBold"
+    }
+    
+    var titilliumWebSemiBold: String {
+        return "TitilliumWeb-SemiBold"
+    }
+    
+}
---
+++ UIFontName/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  UIFontName
+//
+//  Created by andres paladines on 11/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createLabel()
+    }
+
+    func createLabel() {
+        let message =  """
+Example text with a ttf font
+Don't forget the data in the info.plist
+"""
+        let font = UIFont(name: UIFontName.shared.openSansSemiBold, size: 12)
+        let ribbon = UILabel()
+        ribbon.frame = CGRect(x: 0, y: 0, width: 230, height: 40)
+        ribbon.backgroundColor = UIColor.red.withAlphaComponent(0.5)
+        ribbon.textColor = .white
+        ribbon.text = message
+        ribbon.font = font
+        ribbon.textAlignment = .center
+        ribbon.numberOfLines = 0
+        ribbon.center = view.center
+        self.view.addSubview(ribbon)
+    }
+}
+
