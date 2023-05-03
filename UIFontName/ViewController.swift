a8112b14b0d93d645e0953a4cb30d7935b5ca9f0
Andres D. Paladines <andrespaladines@hotmail.com>
2023-05-03 08:30:37 -0400

    Update ViewController.swift

--- UIFontName/ViewController.swift
+++ UIFontName/ViewController.swift
@@ -20,6 +20,7 @@ Example text with a ttf font
 Don't forget the data in the info.plist
 """
         let font = UIFont(name: UIFontName.shared.openSansSemiBold, size: 12)
+        //let font2 = UIFont(customName: .openSansSemiBold, size: 12) // You can use both ways to achieve the same goal
         let ribbon = UILabel()
         ribbon.frame = CGRect(x: 0, y: 0, width: 230, height: 40)
         ribbon.backgroundColor = UIColor.red.withAlphaComponent(0.5)
